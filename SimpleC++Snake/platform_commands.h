1f6b6df9a5e1d532daa3f3bb96de162978b79dbc
Brokn_Apples <trentons708@gmail.com>
2024-04-09 19:17:41 -0500

    forgot one last thing oops

--- SimpleC++Snake/platform_commands.h
+++ SimpleC++Snake/platform_commands.h
@@ -17,10 +17,6 @@ enum { // holds every button action in the game
 	BUTTON_MOVEDOWN,
 	BUTTON_MOVELEFT,
 	BUTTON_MOVERIGHT,
-	BUTTON_PAUSE,
-	BUTTON_LEFTCLICK,
-	BUTTON_RIGHTCLICK,
-	BUTTON_GOBACK,
 
 	// how many buttons actions there are in total
 	BUTTON_COUNT 
